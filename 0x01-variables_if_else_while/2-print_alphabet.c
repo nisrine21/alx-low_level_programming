feeb4a8d2f8cf7ba89f28b504221d213e8960e0c
Nisrine Zaida <nisrinezaida@gmail.com>
2023-03-19 08:34:52 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: Prints the alphabetics 
+ * Description: Prints the alphabetics
  * Return: 0
  */
 int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	putchar (c);
-	putchar ('\n');
-	return(0);
-
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar (c);
+putchar ('\n');
+return (0);
 }
