e727c68e58a471baf8cb1b81dc087f3146834e9f
Nisrine Zaida <125604306+nisrine21@users.noreply.github.com>
2023-03-21 20:44:12 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,18 +8,14 @@
 
 int _islower(int c)
 {
-<<<<<<< HEAD
 	if (c >= 97 && c <= 122)
 	{
 	return (1);
 	}
 	return (0);
-
-=======
 if (c >= 97 && c <= 122)
 {
 return (1);
 }
 return (0);
->>>>>>> 30187922cc439ca57f2bf0d8814762d0fc3e2fd2
 }
