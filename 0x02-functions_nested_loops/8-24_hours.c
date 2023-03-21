e8a4737dc39f207a257ac0dc416c77352a7db38b
Nisrine Zaida <nisrinezaida@gmail.com>
2023-03-21 15:17:28 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@
 
 void jack_bauer(void)
 {
-int a, b, c ,d;
+int a, b, c, d;
 for (a = 0; a <= 2; a++)
 {
 for (b = 0; b <= 9; b++)
@@ -18,11 +18,11 @@ for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
 {
-_putchar('0' + a);
-_putchar('0' + b);
+_putchar(a + '0');
+_putchar(b + '0');
 _putchar(58);
-_putchar('0' + c);
-_putchar('0' + d);
+_putchar(c + '0');
+_putchar(d + '0');
 _putchar('\n');
 }
 }
