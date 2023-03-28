3f8c36c891596c55dea6cab16a88bf8b5c27ecb9
Nisrine Zaida <nisrinezaida@gmail.com>
2023-03-28 04:45:27 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,6 +5,8 @@
 /*
  * reset_to_98 - reset argument to 98
  * @n: pointer
+ * Description: function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
  * Return: void
  */
 
