c4de8b2c851ca119264f07d71dc6a1157ee73b3f
Nisrine Zaida <nisrinezaida@gmail.com>
2023-04-07 07:43:34 -0700

    mandority task

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,23 +1,19 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * _strchr - prints found c
- * @s: pointer to char
- * @c: char params to found
- * Return: *S
-*/
-
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-	}
-	return (0);
+int i = 0;
+for (; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (0);
 }
