a9d8281946e1d660eb7d1e990dac21f96a8ed736
nisrine21 <nisrinezaida@gmail.com>
2023-07-10 09:00:11 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (filename != NULL)
+	if (text_content)
 	{
 		for (n_letters = 0; text_content[n_letters]; n_letters++)
 			;
